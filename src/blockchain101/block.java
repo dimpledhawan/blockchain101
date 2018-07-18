a78eecdaa38fc5daf668aefc15e6a6a2efd45729
dimpledhawan <dhawan.dimple21@gmail.com>
2018-07-17 22:55:51 -0400

    block class

--- src/blockchain101/block.java
+++ src/blockchain101/block.java
@@ -5,10 +5,21 @@
  */
 package blockchain101;
 
+import java.util.Date;
+
 /**
  *
  * @author dimpledhawan
  */
 public class block {
-
+    public String hash;
+    public String prevHash;
+    private String data;
+    private long timeStamp;
+    
+    public block(String data, String prevHash) {
+        this.data = data;
+        this.prevHash = prevHash;
+        this.timeStamp = new Date().getTime();
+    }
 }
