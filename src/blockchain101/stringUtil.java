c17801533b7d2ad7931509ef11f61c3d8209071e
dimpledhawan <dhawan.dimple21@gmail.com>
2018-07-19 20:12:13 -0400

    finish sha256 helper method

--- src/blockchain101/stringUtil.java
+++ src/blockchain101/stringUtil.java
@@ -15,9 +15,15 @@ public class stringUtil {
     public static String applySha256(String input) {
         try {
             MessageDigest digest = MessageDigest.getInstance("SHA-256");
-            byte[] hash = digest.digest(input.getBytes("UTF-8"));
-//            StringBuffer hexString = 
-            return null;
+            byte[] encodedHash = digest.digest(input.getBytes("UTF-8"));
+            StringBuffer hexString = new StringBuffer();
+            for(int i = 0; i < encodedHash.length; i++) {
+                String hex = Integer.toHexString(0xff & encodedHash[i]);
+                if(hex.length() == 1)
+                    hexString.append('0');
+                hexString.append(hex);
+            }
+            return hexString.toString();
         }
         catch(Exception e) {
             throw new RuntimeException(e);
